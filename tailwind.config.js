e3758c8f3efeeab232378f2101353212dec7c838
zhongleiyang <zhongleiyang@foxmail.com>
2023-02-06 15:14:17 +0800

    fix: fix TableOfContent bug，添加scrollMarginTop属性

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,29 @@ module.exports = {
   // 加入 darkMode
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      typography: () => ({
+        DEFAULT: {
+          css: {
+            a: {
+              textDecoration: 'none',
+            },
+            'h1, h2, h3, h4, h5, h6': {
+              position: 'relative',
+              scrollMarginTop: '90px',
+
+              '& a::before': {
+                content: 'none !important',
+              },
+            },
+            img: {
+              margin: '24px auto',
+            },
+          },
+        },
+      }),
+    },
   },
+
   plugins: [require('@tailwindcss/typography')],
 };
