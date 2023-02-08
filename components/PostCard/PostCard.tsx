fa6bceb9b2cd0c0f5d9898f6db5c2adbcc5d1839
zhongleiyang <zhongleiyang@foxmail.com>
2023-02-08 12:07:17 +0800

    feat: add search post feature.

---
+++ components/PostCard/PostCard.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+import Image from '../../components/MDXComponents/Image';
+import { formatDate } from '../../lib/formatDate';
+
+const PostCard = (props) => {
+  const { slug, title, description, updated_at, likes_count } = props;
+
+  return (
+    <Link
+      href={`/blog/${slug}`}
+      className="flex flex-col space-y-3 rounded-2xl border border-accent-2 p-6 transition-all duration-300 hover:scale-105 hover:bg-accent-1"
+    >
+      <Image src="/static/images/cover.png" width={1280} height={720} alt={title} rounded="rounded-lg" />
+
+      <div className="flex-grow space-y-4">
+        <h2 className="text-xl font-bold">{title}</h2>
+        <p className="mt-4 text-accent-5">{description}</p>
+      </div>
+
+      <div className="flex items-center text-sm">
+        {formatDate(updated_at)}
+        &nbsp;/&nbsp;
+        {`${likes_count} likes`}
+        &nbsp;/&nbsp;
+        {`9999 views`}
+      </div>
+    </Link>
+  );
+};
+
+export default PostCard;
--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,53 +1,53 @@
 import Link from 'next/link';
 import React from 'react';
 // import { getAllPosts } from "../lib/mdx";
-import { formatDate } from '../../lib/formatDate';
 import { YuqueApi, Repo, Doc } from '../api/yuque-api';
 
 import ThemeSwitch from '../../components/ThemeSwitch';
 import LayoutWrapper from '../../components/LayoutWrapper';
 
-import { IconSearch } from '@tabler/icons';
-import Image from '../../components/MDXComponents/Image';
+import PostCard from '../../components/PostCard/PostCard';
 
-export default function Home({ docs }) {
-  const postTitle = docs.map((item) => item.title);
-  // console.log(323, postTitle);
-  // https://www.yuque.com/api/zsearch?limit=21&p=1&q=024&scope=blueheart%2Fblqlhn&sence=modal&tab=book&type=content
+import { IconSearch } from '@tabler/icons';
+
+export default function Blog({ docs }) {
+  const [searchValue, setSearchValue] = React.useState('');
+
+  const filteredPosts = docs.filter((post) => post.title.toLowerCase().includes(searchValue.toLowerCase()));
 
   return (
     <>
-      <h1 className="text-6xl font-bold mb-8">Blog Lists</h1>
-      <hr className="my-8" />
       <div className="mb-8 space-y-8">
-        <p className="text-accent-5">已经发布了{docs.length}篇文章了</p>
+        <h1 className="text-6xl font-bold mb-8">Blog Lists</h1>
+        <hr className="my-8" />
+        <p className="text-accent-5">
+          已经发布了{docs.length}篇文章了已经发布了{docs.length}篇文章了 已经发布了{docs.length}篇文章了 已经发布了{docs.length}篇文章了 已经发布了
+          {docs.length}篇文章了 {docs.length}篇文章了{docs.length}篇文章了{docs.length}篇文章了{docs.length}篇文章了{docs.length}篇文章了{docs.length}
+          篇文章了{docs.length}篇文章了{docs.length}篇文章了{docs.length}篇文章了
+        </p>
+        <div className="relative">
+          <input
+            type="text"
+            value={searchValue}
+            onChange={(e) => setSearchValue(e.target.value)}
+            placeholder="Search articles"
+            aria-label="Search articles"
+            className="w-full rounded-md border border-accent-2 bg-hong-bg py-2 px-3 pl-12 transition-colors duration-200 ease-linear focus:border-accent-5 focus:outline-none"
+            id="search"
+          />
+          <label htmlFor="search">
+            <IconSearch className="absolute top-1/2 left-4 -translate-y-1/2" size={20} />
+          </label>
+        </div>
       </div>
 
-      <ul className="grid gap-4 sm:grid-cols-2">
-        {docs.map(({ slug, title, description, updated_at, likes_count }) => (
-          <li key={slug}>
-            <Link
-              href={`/blog/${slug}`}
-              className="flex flex-col space-y-3 rounded-2xl border border-accent-2 p-6 transition-all duration-300 hover:scale-105 hover:bg-accent-1"
-            >
-              <Image src="/static/images/cover.png" width={1280} height={720} alt={title} rounded="rounded-lg" />
+      {!filteredPosts.length && <div className="max-w-5xl text-center text-xl">文章咱没搜到</div>}
 
-              <div className="flex-grow space-y-4">
-                <h2 className="text-xl font-bold">{title}</h2>
-                <p className="mt-4 text-accent-5">{description}</p>
-              </div>
-
-              <div className="flex items-center text-sm">
-                {formatDate(updated_at)}
-                &nbsp;/&nbsp;
-                {`${likes_count} likes`}
-                &nbsp;/&nbsp;
-                {`9999 views`}
-              </div>
-            </Link>
-          </li>
+      <div className="grid gap-4 sm:grid-cols-2">
+        {filteredPosts.map((post) => (
+          <PostCard key={post.slug} {...post} />
         ))}
-      </ul>
+      </div>
     </>
   );
 }
